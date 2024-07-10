7354758d8f46851766708559d1c1a9c30b2a7a04
tabbytadi <adi_stankova@abv.bg>
2024-07-10 11:36:44 +0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,84 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { MenubarModule } from 'primeng/menubar';
+import { MenuItem } from 'primeng/api';
+import { DropdownModule } from 'primeng/dropdown';
+
+interface City {
+  name: string;
+  code: string;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule, MenubarModule, ReactiveFormsModule, DropdownModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'all-primeng-components';
+export class AppComponent implements OnInit {
+  cities: City[] = [
+    { name: 'New York', code: 'NY' },
+    { name: 'Rome', code: 'RM' },
+    { name: 'London', code: 'LDN' },
+    { name: 'Istanbul', code: 'IST' },
+    { name: 'Paris', code: 'PRS' }
+  ];
+
+  formGroup: FormGroup = new FormGroup({
+    selectedCity: new FormControl<City | null>(null)
+  });
+
+  items: MenuItem[] | undefined;
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Home',
+        icon: 'pi pi-home'
+      },
+      {
+        label: 'Features',
+        icon: 'pi pi-star'
+      },
+      {
+        label: 'Projects',
+        icon: 'pi pi-search',
+        items: [
+          {
+            label: 'Components',
+            icon: 'pi pi-bolt'
+          },
+          {
+            label: 'Blocks',
+            icon: 'pi pi-server'
+          },
+          {
+            label: 'UI Kit',
+            icon: 'pi pi-pencil'
+          },
+          {
+            label: 'Templates',
+            icon: 'pi pi-palette',
+            items: [
+              {
+                label: 'Apollo',
+                icon: 'pi pi-palette'
+              },
+              {
+                label: 'Ultima',
+                icon: 'pi pi-palette'
+              }
+            ]
+          }
+        ]
+      },
+      {
+        label: 'Contact',
+        icon: 'pi pi-envelope'
+      }
+    ];
+  }
 }
